9125cc91aebfb2b4c9a12c9aa0049f21576b1a79
Caoimhan Bradley <caoimhan@alchemytechs.com>
2019-08-01 16:28:17 +0100

    Add project files.

---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects //constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry Potter", "Harry Potter", 400);
+            Book book2 = new Book("Lord Of The Rings", "Tolkein", 700);
+            Book book3 = new Book("A Game Of Thrones", "George R.R Martin", 800);
+
+            Console.WriteLine(book2.title);
+
+            Console.ReadLine();
+        }
+    }
+}
